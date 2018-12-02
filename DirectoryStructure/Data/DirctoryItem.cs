d1960898936ca4c05a4b032dfdc437f5ff1b95e2
Mani <calmsea_22@yahoo.com>
2018-12-02 23:01:03 +0100

    ...

---
+++ DirectoryStructure/Data/DirctoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfTreeView.DirectoryStructure.Data {
+    class DirctoryItem {
+        /// <summary>
+        /// Type of the directory item
+        /// </summary>
+        public DrirectoryItemType Type { get; set; }
+        /// <summary>
+        /// The absolute path to this directory item
+        /// </summary>
+        public String FullPath { get; set; }
+        /// <summary>
+        /// The name of this directory item
+        /// </summary>
+        public String Name { get { return Type == DrirectoryItemType.drive ? FullPath : DirectoryStructure.GetFileFolderName(FullPath); } }
+
+
+    }
+}
---
+++ DirectoryStructure/Data/DirectoryItemType.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace WpfTreeView.DirectoryStructure.Data {
+    /// <summary>
+    /// The Type of the directory item
+    /// </summary>
+    public enum DrirectoryItemType{
+
+        file,
+        drive,
+        folder
+    }
+}
